7d62e5d24b944bf4e77ba223dd66fb5b6d929780
Örjan Persson <orjan@spotify.com>
2013-09-03 22:24:02 -0700

    Added local performance program for latency
    Inspired by the counterpart found in nanomsg.

---
+++ perf/local_lat/local_lat.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"syscall"
+
+	"github.com/op/zenio"
+)
+
+var (
+	debug          = flag.Bool("debug", true, "debug protocol")
+	scheme         = flag.String("scheme", "sp+tcp", "transport scheme to use")
+	host           = flag.String("host", "127.0.0.1:4242", "host:port to bind to")
+	messageSize    = flag.Int("message-size", 1024, "message size to receive")
+	roundtripCount = flag.Int("roundtrip-count", 1, "number of roundtrips")
+)
+
+func main() {
+	flag.Parse()
+
+	ln, err := zenio.Listen(*scheme, *host, *debug)
+	if err != nil {
+		panic(err)
+	}
+	defer ln.Close()
+
+	var msg zenio.BytesMessage
+	for i := 0; i < *roundtripCount; i++ {
+		if err := ln.Recv(&msg); err != nil {
+			if err != io.EOF {
+				panic(err)
+			}
+		}
+
+		if err := ln.Send(&msg); err != nil {
+			op, ok := err.(*net.OpError)
+			if ok {
+				switch op.Err {
+				case syscall.EPIPE:
+					fmt.Printf("| broken pipe\n")
+					return
+				case syscall.ECONNRESET:
+					fmt.Printf("| connection reset\n")
+					return
+				}
+			}
+			fmt.Printf("%#v\n", err)
+			panic(err)
+		}
+	}
+}
